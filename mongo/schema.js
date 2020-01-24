2ba0989ceb330df16e45cefc6e2afc0671e6af62
Yosua Simatupang <yosua@dkatalis.com>
2020-01-24 09:18:05 +0700

    resize picture

--- mongo/schema.js
+++ mongo/schema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 const mongoUrl = 'mongodb://apiAdmin:admin123@clusterbp-shard-00-00-mrpsm.mongodb.net:27017,clusterbp-shard-00-01-mrpsm.mongodb.net:27017,clusterbp-shard-00-02-mrpsm.mongodb.net:27017/belanjapedia?ssl=true&replicaSet=ClusterBP-shard-0&authSource=admin&retryWrites=true&w=majority'
 
-
 const server = async () => {
   mongoose.connect(mongoUrl, {
     useNewUrlParser: true,
